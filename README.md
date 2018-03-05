d2260a9c4bb5eee8e372bf8bee16f0345dc4c5bb
Dennis <dennis.kawurek@hotmail.de>
2018-03-05 01:52:16 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,35 @@
+# Deploy flask to uberspace
+
+A small executable bash-script to deploy your flask application fast (mainly intendet for APIs).
+
+## How to use
+
+1. Upload your flask application to your uberspace server
+2. Upload the deploy_flask.sh to your uberspace server and make it executable
+3. In the terminal install the flask and flup6 with the commands:
+
+```
+pip3 install flask --user
+pip3 install flup6 --user
+```
+
+4. Execute deploy_flask.sh and follow the instructions
+
+```
+./deploy_flask.sh
+```
+
+You can reach your flask application in the path /fcgi-bin/your_application_name.fcgi
+
+## ToDos
+
+- Implement rewrite rules for a more beautiful URL
+- Don't exit the script after every false input - ask again for
+- Test on Uberspace V7
+- Clean the code
+
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ deploy_flask.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+echo "Your Uberspace username?"
+read UBERSPACE_NAME
+
+if [ -z UBERSPACE_NAME ]; then
+        echo "Uberspace name must not be empty."
+        exit
+fi
+
+echo "Location of your Flask application? (Type in full location path e.g. /home/username/flaskapplication)"
+read FLASK_LOCATION
+
+if [ -z FLASK_LOCATION ]; then
+        echo "Location of Flask application must not be empty."
+        exit
+fi
+
+echo "Name of the file where your entry point of the application is (without .py)?"
+read FLASK_MODULE
+
+if [ -z FLASK_MODULE ]; then
+        echo "MODULE of Flask application must not be empty."
+        exit
+fi
+
+echo "Name of your Flask application?"
+read FLASK_NAME
+
+if [ -z FLASK_NAME ]; then
+        echo "Location of Flask application must not be empty."
+        exit
+fi
+
+echo "Identifier of your application? (e.g. if https://www.site.de/api you need to write 'api')"
+read API_URL
+
+if [ -z API_URL ]; then
+        echo "The."
+        exit
+fi
+
+cd /home/$UBERSPACE_NAME/fcgi-bin
+
+if [ -e $API_URL.fcgi ]; then
+        echo "The API-URL already exists. Please check the corresponding $API_URL.fcgi file in your /fcgi-bin folder"
+        exit
+fi
+
+touch $API_URL.fcgi
+
+/bin/cat <<EOM >$API_URL.fcgi
+#!/usr/bin/env python3
+import sys
+
+sys.path.insert(0, "$FLASK_LOCATION")
+from $FLASK_MODULE import $FLASK_NAME
+
+from flup.server.fcgi_fork import WSGIServer
+WSGIServer($FLASK_NAME).run()
+EOM
+
+chmod +x $API_URL.fcgi
+
+echo "Well done! You can reach your flask application under domain.com/fcgi-bin/$API_URL.fcgi"
+exit 1
